914493f47e33ad666cb4fa1d3e35eababb3f6727
yassarannabil <yassarannabil1@gmail.com>
2026-06-18 14:56:07 +0700

    Fix NameError IsAdminUser in api_dashboard

--- core/api_dashboard.py
+++ core/api_dashboard.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAdminUser
 from .models import LiveInventory, MasterCourier, Transaction, Customer, Box
 from .serializers import LiveInventorySerializer
 from rest_framework import serializers
